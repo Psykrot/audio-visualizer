61638f0db1f2d455127cb1605fc44859d1105e0f
Psykrot <psykrot@hotmail.com>
2025-10-28 10:14:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
-// Check if access_token is in URL hash
-function getAccessTokenFromHash() {
+window.addEventListener("DOMContentLoaded", () => {
+  // Parse access token from URL hash
   const hash = window.location.hash.substring(1); // remove #
   const params = new URLSearchParams(hash);
-  return params.get("access_token");
-}
+  const accessToken = params.get("access_token");
 
-let accessToken = getAccessTokenFromHash();
+  const loginButton = document.getElementById("login-button");
+  const visualizerContainer = document.getElementById("visualizer");
 
-const loginButton = document.getElementById("login-button");
-const visualizerContainer = document.getElementById("visualizer");
+  if (accessToken) {
+    // Hide login button
+    if (loginButton) loginButton.style.display = "none";
 
-// If we have a token, hide login button and start visualizer
-if (accessToken) {
-  if (loginButton) loginButton.style.display = "none";
-  startVisualizer(accessToken);
-} else {
-  // Show login button
-  if (loginButton) {
-    loginButton.style.display = "block";
-    loginButton.addEventListener("click", () => {
-      // Redirect to your backend login
-      window.location.href = "https://auth.avinylmix.com/login";
-    });
+    // Clear URL hash so it doesn't show in address bar
+    history.replaceState(null, "", window.location.pathname);
+
+    // Start visualizer
+    startVisualizer(accessToken);
+  } else {
+    // Show login button
+    if (loginButton) {
+      loginButton.style.display = "block";
+      loginButton.addEventListener("click", () => {
+        // Redirect to backend login
+        window.location.href = "https://auth.avinylmix.com/login";
+      });
+    }
   }
-}
 
-// Function to start visualizer (example)
-function startVisualizer(token) {
-  console.log("Access token:", token);
+  function startVisualizer(token) {
+    console.log("Spotify access token:", token);
 
-  // TODO: replace this with your audio visualizer logic
-  visualizerContainer.innerHTML = `<p style="color:white;">Visualizer started with Spotify token!</p>`;
+    // Example placeholder
+    visualizerContainer.innerHTML = `<p style="color:white;">Visualizer started!</p>`;
 
-  // Example: you could fetch currently playing track:
-  // fetch("https://api.spotify.com/v1/me/player/currently-playing", {
-  //   headers: { Authorization: `Bearer ${token}` }
-  // }).then(res => res.json()).then(console.log);
-}
+    // TODO: fetch currently playing track or start audio visualizer
+    // fetch("https://api.spotify.com/v1/me/player/currently-playing", {
+    //   headers: { Authorization: `Bearer ${token}` }
+    // }).then(res => res.json()).then(console.log);
+  }
+});
